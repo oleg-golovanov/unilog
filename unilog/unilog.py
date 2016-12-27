8488f89d954810fc95dc0bf04d95496b9a6736e1
Igor Kulyabin <kuligleo@gmail.com>
2016-12-27 16:07:37 +0500

    Changed function parameters.

--- unilog/unilog.py
+++ unilog/unilog.py
@@ -4,37 +4,37 @@
 import convert
 
 
-def as_unicode(obj, pretty=False, encoding=convert.LOCALE):
+def as_unicode(obj, encoding=convert.LOCALE, pretty=False):
     """
     Representing any object to unicode string.
 
     :param obj: any object
-    :type pretty: bool
-    :param pretty: Pretty print.
     :type encoding: str
     :param encoding: codec for encoding unicode strings
                      (locale.getpreferredencoding() by default)
+    :type pretty: bool
+    :param pretty: Pretty print.
 
     :rtype: unicode
     :return: any object as unicode string
     """
 
-    return convert.convert(obj, 0 if pretty else None, encoding)
+    return convert.convert(obj, encoding, 0 if pretty else None)
 
 
-def as_str(obj, pretty=False, encoding=convert.LOCALE):
+def as_str(obj, encoding=convert.LOCALE, pretty=False):
     """
     Representing any object to string.
 
     :param obj: any object
-    :type pretty: bool
-    :param pretty: Pretty print.
     :type encoding: str
     :param encoding: codec for encoding unicode strings
                      (locale.getpreferredencoding() by default)
+    :type pretty: bool
+    :param pretty: Pretty print.
 
     :rtype: str
     :return: any object as string
     """
 
-    return as_unicode(obj, pretty, encoding).encode(encoding)
+    return as_unicode(obj, encoding, pretty).encode(encoding)
