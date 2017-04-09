c5b235142057454a7b498d324cdbbeefa3a89626
Oleg Golovanov <golovanov.ov@gmail.com>
2017-04-10 04:06:53 +0500

    Tests for python3.

--- tests/tests.py
+++ tests/tests.py
@@ -5,6 +5,7 @@ import datetime
 import unittest
 
 import unilog.convert
+import unilog.compat
 
 
 class TestClass(object):
@@ -17,6 +18,14 @@ class TestClass(object):
 class UniTest(unittest.TestCase):
 
     def test_iterable(self):
+        values = {
+            'date': u"u'2016-12-06'",
+            'datetime': u"u'2016-12-06 11:22:33.444444'",
+            'unicode': u"u'пункт3'"
+        }
+        if unilog.compat.PY33:
+            values = {k: v.replace('u', '') for k, v in values.items()}
+
         self.assertEqual(
             unilog.as_unicode(
                 [
@@ -33,28 +42,36 @@ class UniTest(unittest.TestCase):
                     False
                 ]
             ),
-            u"[u'2016-12-06', u'2016-12-06 11:22:33.444444', 'item1', 'пункт2', "
-            u"u'пункт3', 4, 4.44, b'\\\\x00\\\\x01\\\\x02', None, True, False]"
+            u"[{date}, {datetime}, 'item1', 'пункт2', {unicode}, 4, 4.44, "
+            u"b'\\x00\\x01\\x02', None, True, False]".format(**values)
         )
 
     def test_mapping(self):
-        generator = (i for i in xrange(10))
+        generator = (i for i in unilog.compat.XRangeType(10))
+        result = (
+            "{{\n"
+            "    'generators': [\n"
+            "        '{!r}',\n"
+            "        '{}'\n"
+            "    ]\n"
+            "}}".format(
+                generator, 'range(0, 10)' if unilog.compat.PY33 else 'xrange(10)'
+            )
+        )
+        if unilog.compat.PY33:
+            result = result.encode(unilog.convert.LOCALE)
+
         self.assertEqual(
             unilog.as_str(
                 {
                     'generators': [
                         generator,
-                        xrange(10)
+                        unilog.compat.XRangeType(10)
                     ]
                 },
                 pretty=True
             ),
-            "{{\n"
-            "    'generators': [\n"
-            "        '{!r}',\n"
-            "        'xrange(10)'\n"
-            "    ]\n"
-            "}}".format(generator)
+            result
         )
 
     def test_register_converter(self):
@@ -62,6 +79,7 @@ class UniTest(unittest.TestCase):
             TestClass,
             lambda x: [x.test1, x.test2]
         )
+
         self.assertEqual(
             unilog.as_unicode(TestClass()),
             u"['test1', 2]"
