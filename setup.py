ab03cc2abacd54c4c7ff167defe50dcb0229b90e
Oleg Golovanov <golovanov.ov@gmail.com>
2017-04-11 01:30:41 +0500

    Update classifiers in setup.py.

--- setup.py
+++ setup.py
@@ -28,11 +28,16 @@ setup(
     long_description=open(os.path.join(DIR, 'README.rst')).read(),
     test_suite='tests.tests',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
