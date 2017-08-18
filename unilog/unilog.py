5294b2d817daec180bc95537f482ca8688035bbc
Oleg Golovanov <golovanov.ov@gmail.com>
2017-08-18 18:44:10 +0500

    Update docstrings.

--- unilog/unilog.py
+++ unilog/unilog.py
@@ -6,14 +6,14 @@ from . import convert
 
 def as_unicode(obj, encoding=convert.LOCALE, pretty=False):
     """
-    Representing any object to unicode string.
+    Representing any object to <unicode> string (python2.7) or <str> string (python3.0).
 
     :param obj: any object
     :type encoding: str
     :param encoding: codec for encoding unicode strings
                      (locale.getpreferredencoding() by default)
     :type pretty: bool
-    :param pretty: Pretty print.
+    :param pretty: pretty print
 
     :rtype: unicode
     :return: any object as unicode string
@@ -24,14 +24,14 @@ def as_unicode(obj, encoding=convert.LOCALE, pretty=False):
 
 def as_str(obj, encoding=convert.LOCALE, pretty=False):
     """
-    Representing any object to string.
+    Representing any object to <str> string (python2.7) or <bytes> string (python3.0).
 
     :param obj: any object
     :type encoding: str
     :param encoding: codec for encoding unicode strings
                      (locale.getpreferredencoding() by default)
     :type pretty: bool
-    :param pretty: Pretty print.
+    :param pretty: pretty print
 
     :rtype: str
     :return: any object as string
